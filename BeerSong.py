b18bee7db0ce7f30770f688ef9dd8c62b9546795
matthew <mldemott@oakland.edu>
2017-01-18 14:28:16 -0500

    Adding functionality to Main and Ninety_Nine_Bottles_of_Beer methods.

--- BeerSong.py
+++ BeerSong.py
@@ -1,7 +1,17 @@
+from locale import str
+
+from __builtin__ import range
+
 def  Ninety_Nine_Bottles_of_Beer():
-    return 0
+    for i in range(99, 0, -1):
+        if i == 1:
+            print(
+            "1 bottle of beer on	the	wall, 1 bottle of beer take	one	down, pass it around, no more bottles of beer on the wall.")
+        else:
+            print(str(i) + " bottle of beer on the wall, 1 bottle of beer take one down, pass it around, "
+                  + str(i - 1) + " bottles of beer on the wall.")
 
 def main():
-    return 0
+    Ninety_Nine_Bottles_of_Beer()
 
 main()
